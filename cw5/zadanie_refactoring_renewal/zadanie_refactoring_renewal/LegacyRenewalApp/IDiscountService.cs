01a5ac1db472d3ff19ab9fcfcaff1235c5ede536
Maciej Guevara Braun <s33140@pjwstk.edu.pl>
2026-04-14 18:00:30 +0200

    Dodanie glownego sposobu przydzielania znizek

--- cw5/zadanie_refactoring_renewal/zadanie_refactoring_renewal/LegacyRenewalApp/IDiscountService.cs
+++ cw5/zadanie_refactoring_renewal/zadanie_refactoring_renewal/LegacyRenewalApp/IDiscountService.cs
@@ -2,5 +2,7 @@ namespace LegacyRenewalApp;
 
 public interface IDiscountService
 {
-    
+    (decimal TotalDiscount, string Note) CalculateDiscount(Customer customer, SubscriptionPlan subscriptionPlan, decimal amount,
+        int seatCount,
+        bool useLoyaltyPoints);
 }
